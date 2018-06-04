350a4185e7c030f3b2cf40a8eca070d0f55b35a8
mdekauwe <mdekauwe@gmail.com>
2018-06-04 11:10:03 +1000

    Fixed last year issue

--- generate_average_summer_tmax.sh
+++ generate_average_summer_tmax.sh
@@ -4,12 +4,16 @@
 
 cdo mergetime raw_data/TMP/cru_ts4.00*.nc raw_data/TMP/merged_TMP_1961_2010.nc
 
+# Delete final year as it isn't complte and will mess up seasonal calcs
+cdo delete,year=2010 raw_data/TMP/merged_TMP_1961_2010.nc raw_data/TMP/merged_TMP_1961_2009.nc
+rm raw_data/TMP/merged_TMP_1961_2010.nc
+
 # calculate monthly max across all years
 
-cdo ymonmax raw_data/TMP/merged_TMP_1961_2010.nc raw_data/TMP/merged_TMAX_1961_2010.nc
+cdo ymonmax raw_data/TMP/merged_TMP_1961_2009.nc raw_data/TMP/merged_TMAX_1961_2009.nc
 
 # calculate seasonal mean
 
-cdo seasmean raw_data/TMP/merged_TMAX_1961_2010.nc raw_data/TMP/merged_TMAX_seasmean_1961_2010.nc
+cdo seasmean raw_data/TMP/merged_TMAX_1961_2009.nc raw_data/TMP/merged_TMAX_seasmean_1961_2009.nc
 
-rm raw_data/TMP/merged_TMAX_1961_2010.nc raw_data/TMP/merged_TMP_1961_2010.nc
+rm raw_data/TMP/merged_TMAX_1961_2009.nc raw_data/TMP/merged_TMP_1961_2009.nc
