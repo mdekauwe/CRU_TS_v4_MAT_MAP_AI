58fc3e29ba88e0472287c2c9eca9f0b2e0bbb042
mdekauwe <mdekauwe@gmail.com>
2018-06-04 10:46:06 +1000

    Fixed syntax bug

--- generate_average_summer_tmax.sh
+++ generate_average_summer_tmax.sh
@@ -2,7 +2,7 @@
 
 # combine all time slices into one file
 
-cdo mergetime raw_data/TMP*.nc raw_data/TMP/merged_TMP_1961_2010.nc
+cdo mergetime raw_data/TMP/cru_ts4.00*.nc raw_data/TMP/merged_TMP_1961_2010.nc
 
 # calculate monthly max across all years
 
